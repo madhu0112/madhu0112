9ad19b7d8d78b14645b4c773675e826334aca793
madhu0112 <madhurabiligiri01@gmail.com>
2026-03-16 22:21:43 +0530

    Delete w directory

--- w/track.php
+++
@@ -1,9 +0,0 @@
-<?php
-$counterFile = "counter.txt";
-if (!file_exists($counterFile)) {
- file_put_contents($counterFile, "0");
-}
-$currentCount = file_get_contents($counterFile);
-$newCount = $currentCount + 1;
-file_put_contents($counterFile, $newCount);
-?>
